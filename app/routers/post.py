5f797277fbab2c9d2ffadf29bfaaa2c72ee159c8
Syed Shahzad Abdul Wajid <shahzadksa777@gmail.com>
2026-01-05 23:13:41 -0500

    updated posts logic

--- app/routers/post.py
+++ app/routers/post.py
@@ -12,9 +12,7 @@ router = APIRouter(
 
 @router.get("/", response_model=list[schemas.PostOut])
 async def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
-    # cursor.execute("""SELECT * FROM posts""")
-    # posts = cursor.fetchall()
-    # posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+
     
     posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
         models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
@@ -22,10 +20,6 @@ async def get_posts(db: Session = Depends(get_db), current_user: int = Depends(o
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-        # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
-        #             (post.title, post.content, post.published))
-        # new_post = cursor.fetchone()
-        # conn.commit()
         new_post = models.Post(**post.dict(), owner_id=current_user.id)
         db.add(new_post)
         db.commit()
@@ -34,12 +28,6 @@ async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), c
 
 @router.get("/{id}", response_model=schemas.PostOut)
 def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    # cursor.execute(""" SELECT * FROM posts WHERE id = %s""", (str(id),))
-    # post = cursor.fetchone()
-    # if not post:
-    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-    #                         detail=f"Post with id: {id} was not found")
-    # post = db.query(models.Post).filter(models.Post.id == id).first()
     post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
         models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
     if not post:
@@ -50,9 +38,6 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    # cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
     if post is None:
@@ -68,11 +53,6 @@ def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depe
 
 @router.put("/{id}", response_model=schemas.Post)
 def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    # cursor.execute(""" UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
-    #                (post.title, post.content, post.published, str(id))
-    #                )
-    # updated_post = cursor.fetchone()
-    # conn.commit()
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
 
